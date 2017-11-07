017e373594e7952a6026ec7cfc9ff703cd888794
XZ <XZ@MyWifiNoPassword.local>
2017-11-07 21:58:04 +0800

    首页一点点

---
+++ XZDouYuLive/Classes/Home/Controller/XZHomeController.swift
@@ -0,0 +1,32 @@
+//
+//  XZHomeController.swift
+//  XZDouYuLive
+//
+//  Created by XZ on 2017/11/6.
+//  Copyright © 2017年 RongTuoJinRong. All rights reserved.
+//
+
+import UIKit
+
+class XZHomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 设置UI界面
+        setupUI()
+        
+    }
+}
+
+// MARK:- 设置UI界面
+extension XZHomeController {
+    private func setupUI() {
+        // 1.设置导航栏
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        
+    }
+}
